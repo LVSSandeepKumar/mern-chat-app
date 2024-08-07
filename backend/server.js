cd546fc5a2ce7877d63e52b1d6776877f23b7b18
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-07 10:45:27 +0530

    Pre deployment commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { ENV_VARS } from "./utils/envVars.js";
 import { connectDB } from "./utils/connectDB.js";
@@ -11,14 +12,21 @@ import userRoutes from "./routes/user.route.js";
 
 const PORT = ENV_VARS.PORT;
 
+const __dirname = path.resolve(); 
 // middleware functions
 app.use(express.json()); // to parse incoming requests with JSON payloads
 app.use(cookieParser()); // to read cookies from request headers 
+
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); // to use dist folder in frontend as static file
 
 // routes for various utilities
 app.use("/api/auth", authRoutes);        // authentication routes
 app.use("/api/messages", messageRoutes); // message routes
 app.use("/api/users", userRoutes);       // user routes
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectDB();
